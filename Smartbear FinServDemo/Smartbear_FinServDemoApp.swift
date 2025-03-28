4cf8a018ff6d0cfc59c090847e639d9f76d53972
Gary Wood <gary.wood@smarbear.com>
2025-03-28 09:27:22 -0500

    Initial Commit

---
+++ Smartbear FinServDemo/Smartbear_FinServDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Smartbear_FinServDemoApp.swift
+//  Smartbear FinServDemo
+//
+//  Created by Gary Wood on 3/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Smartbear_FinServDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Smartbear FinServDemoTests/Smartbear_FinServDemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  Smartbear_FinServDemoTests.swift
+//  Smartbear FinServDemoTests
+//
+//  Created by Gary Wood on 3/28/25.
+//
+
+import Testing
+@testable import Smartbear_FinServDemo
+
+struct Smartbear_FinServDemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
